faf127d98f9e74320998ba5d8f83f2605b5f47f0
Adham Asharf <139995772+Adham-3amer@users.noreply.github.com>
2025-09-06 12:53:21 +0300

    Add files via upload

---
+++ Logical Operators Homework/homework_02.cpp
@@ -0,0 +1,29 @@
+/*
+Homework 2: Create logic!
+
+Write a program that reads 3 integers about the classroom:
+
+Number of boys (nb)
+
+Number of girls (ng)
+
+Number of teachers (nt)
+
+Prepare and print a boolean variable for these cases:
+
+nb greater than 25
+
+ng less than or equal to 30
+
+nb > 20 and nt > 2 or ng > 30 and nt > 4
+
+Either nb < 60 or ng < 70
+
+Neither nb >= 60 nor ng >= 70
+
+nb is 10 more students than ng
+
+Difference between nb and ng is more than 10 or nt > 5
+
+Either nb is 10 more students than ng or ng is 15 more students than nb
+*/
